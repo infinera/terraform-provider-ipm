6fd05a6e4c3a73bf1fd62670b5299022debdba1a
lam hoang <lhoang@infinera.com>
2023-12-18 12:32:26 -0800

    updated

--- internal/ipm_pf/auth.go
+++ internal/ipm_pf/auth.go
@@ -13,7 +13,7 @@ import (
 // SignIn - Get a new token for user
 func (c *Client) SignIn() (*AuthResponse, error) {
 
-	fmt.Printf("SignIn: host = %s, user= %s, password = %s", c.HostURL, c.Auth.Username, c.Auth.Password)
+	fmt.Printf("SignIn: host = %s, user= %s", c.HostURL, c.Auth.Username)
 	if c.Auth.Username == "" || c.Auth.Password == "" {
 		return nil, fmt.Errorf("please specify username and password for IPM server")
 	}
@@ -22,29 +22,29 @@ func (c *Client) SignIn() (*AuthResponse, error) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // Self signed certificate flag, TODO to remove or data drive it latter
 
 	url := "https://" + c.HostURL +"/realms/xr-cm/protocol/openid-connect/token"
-  method := "POST"
+    method := "POST"
 
 	payload := strings.NewReader("username="+ c.Auth.Username+"&password="+c.Auth.Password+"&grant_type=password&client_secret=xr-web-client&client_id=xr-web-client")
 
-  req, err := http.NewRequest(method, url, payload)
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-  res, err := c.HTTPClient.Do(req)
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
-  defer res.Body.Close()
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
 
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 
 	//str1 := fmt.Sprintf("%s", body)
 	//fmt.Println("body auth" + str1)
